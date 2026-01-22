48af5fc54610fe70164e1fe30e909b04d58eb5c3
richardblondet <richardblondet@gmail.com>
2026-01-22 16:28:02 -0400

    chore: [rb-develop-01] fixing output Changes to be committed: 	modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  output: 'standalone'
 }
 
 export default nextConfig
