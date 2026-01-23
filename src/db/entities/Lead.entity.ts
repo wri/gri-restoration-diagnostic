bb83d37b7a6ff8b4e50b61aa29b9a8903c616a9a
richardblondet <richardblondet@gmail.com>
2026-01-23 10:28:58 -0400

    feat: [RD-5]. Changes to be committed: - modified:   .gitignore (updated to house the .env.example) - new file:   src/db/data-source.ts - new file:   src/db/entities/Assessment.entity.ts - new file:   src/db/entities/Diagnostic.entity.ts - new file:   src/db/entities/Lead.entity.ts - new file:   src/db/entities/Region.entity.ts - new file:   src/db/entities/index.ts - new file:   src/db/schema.dbml

---
+++ src/db/entities/Lead.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Assessment } from './Assessment.entity';
+
+@Entity('lead')
+export class Lead {
+  @PrimaryColumn('varchar', { length: 36 })
+  lead_id!: string;
+
+  @Column('varchar', { nullable: true })
+  job_title?: string;
+
+  @Column('varchar')
+  name!: string;
+
+  @Column('varchar', { unique: true })
+  email!: string;
+
+  @Column('varchar', { nullable: true })
+  organization?: string;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+
+  @OneToMany(() => Assessment, assessment => assessment.lead)
+  assessments!: Assessment[];
+}
---
+++ src/db/entities/Region.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Assessment } from './Assessment.entity';
+
+@Entity('region')
+export class Region {
+  @PrimaryColumn('varchar', { length: 36 })
+  region_id!: string;
+
+  @Column('varchar')
+  region_name!: string;
+
+  @Column('varchar')
+  geography_type!: string;
+
+  @Column('text', { nullable: true })
+  countries?: string;
+
+  @Column('varchar', { nullable: true })
+  sub_region?: string;
+
+  @Column('text')
+  ecosystems!: string; // JSON stringified array
+
+  @Column('text', { nullable: true })
+  gis_url?: string;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+
+  @OneToMany(() => Assessment, assessment => assessment.region)
+  assessments!: Assessment[];
+}
