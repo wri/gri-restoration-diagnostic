f676d1a71183eca119b439c68c603e5dd3041077
AlejoYarce <alejo.yarce990@gmail.com>
2026-02-24 14:51:43 -0500

    [RD-56] - Implement a global navigation bar and a language context

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -2,11 +2,11 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { designSystemStyles } from "@worldresources/wri-design-systems";
-
+import { LanguageProvider } from '@/contexts/LanguageContext'
 
 const Providers = ({ children }: { children: React.ReactNode }) => (
   <ChakraProvider value={designSystemStyles}>
-    {children}
+    <LanguageProvider>{children}</LanguageProvider>
   </ChakraProvider>
 )
 
