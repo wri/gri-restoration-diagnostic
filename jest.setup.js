4e9011d1b33368736e5c53a727c34504e1b86eaf
richardblondet <richardblondet@gmail.com>
2026-02-26 12:25:36 -0400

    modified:   jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,24 @@
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+import { webcrypto } from 'crypto';
 
 // Mock Next.js server-side globals
 if (typeof global.Request === 'undefined') {
   global.Request = class Request {};
   global.Response = class Response {};
   global.Headers = class Headers {};
+}
+
+// Polyfill TextEncoder/TextDecoder for tests
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+// Polyfill Web Crypto API for tests
+if (!global.crypto) {
+  global.crypto = webcrypto;
+}
+
+// Ensure crypto.subtle is available
+if (global.crypto && !global.crypto.subtle) {
+  global.crypto.subtle = webcrypto.subtle;
 }
