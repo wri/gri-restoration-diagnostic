9971d6ef6aafb34a257d21ee6262dc6faa373f8c
kinshuk <kinshuk@gmail.com>
2026-03-09 08:45:43 -0700

    Single NAT gateway (#59)
    To reduce costs

--- terraform/infrastructure/vpc.tf
+++ terraform/infrastructure/vpc.tf
@@ -127,24 +127,24 @@ resource "aws_subnet" "private" {
 # =============================================================================
 
 resource "aws_eip" "nat" {
-  count  = local.create_vpc ? var.availability_zones_count : 0
+  count  = local.create_vpc ? 1 : 0
   domain = "vpc"
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-nat-eip-${count.index + 1}"
+    Name = "${var.project_name}-${var.environment}-nat-eip"
   }
 
   depends_on = [aws_internet_gateway.main]
 }
 
 resource "aws_nat_gateway" "main" {
-  count = local.create_vpc ? var.availability_zones_count : 0
+  count = local.create_vpc ? 1 : 0
 
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
+  allocation_id = aws_eip.nat[0].id
+  subnet_id     = aws_subnet.public[0].id
 
   tags = {
-    Name = "${var.project_name}-${var.environment}-nat-${count.index + 1}"
+    Name = "${var.project_name}-${var.environment}-nat"
   }
 
   depends_on = [aws_internet_gateway.main]
@@ -185,7 +185,7 @@ resource "aws_route_table" "private" {
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main[count.index].id
+    nat_gateway_id = aws_nat_gateway.main[0].id
   }
 
   tags = {
