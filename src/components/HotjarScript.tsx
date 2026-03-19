6afe9c2d8f7dec19df4450d51873249393557266
richardblondet <richardblondet@gmail.com>
2026-03-18 22:03:43 -0400

    restoring lost files in merge conflict theirs vs ours

--- src/components/HotjarScript.tsx
+++ src/components/HotjarScript.tsx
@@ -5,12 +5,8 @@ import Script from 'next/script';
 export function HotjarScript() {
   // More: https://gfw.atlassian.net/browse/RD-75
   // Production: 6664922, QA: 6664921
-  const environment =
-    process.env.RD_APP_ENV || process.env.NEXT_PUBLIC_ENVIRONMENT || process.env.NODE_ENV || 'production';
-  console.log("🚀 ~ HotjarScript ~ environment:", environment)
-  const hjid =
-    process.env.NEXT_PUBLIC_HOTJAR_SITE_ID ||
-    (environment === 'production' ? '6664922' : '6664921');
+  const environment = process.env.NEXT_PUBLIC_ENVIRONMENT || 'production';
+  const hjid = (environment === 'production' ? '6664922' : '6664921');
 
   return (
     <Script
