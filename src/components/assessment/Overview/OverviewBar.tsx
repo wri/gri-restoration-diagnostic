50dbf4d2fc6cea4fa6a96313a78f539ba26d6a41
richardblondet <richardblondet@gmail.com>
2026-03-24 18:21:11 -0400

    clientsite components hydration fixes

--- src/components/assessment/Overview/OverviewBar.tsx
+++ src/components/assessment/Overview/OverviewBar.tsx
@@ -1,10 +1,20 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import ExportResponses from './ExportResponses'
 import { useTranslations } from '@/i18n/useTranslations'
 
 const OverviewBar = ({ assessmentId }: { assessmentId: string }) => {
   const t = useTranslations()
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return <div className='h-12' />
+  }
 
   return (
     <div className='h-11 px-4 border-b border-neutral-400 mb-16 sticky top-0 bg-white z-10 flex items-center justify-between'>
